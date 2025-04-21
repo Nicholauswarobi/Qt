0cf85f1bb25f1a506abbcf1745eb52821bf9caef
Nicholauswarobi <warobinicholaus@gmail.com>
2025-04-22 01:09:51 +0300

    Currency Converter

--- Currency_Converter/mainwindow.cpp
+++ Currency_Converter/mainwindow.cpp
@@ -30,8 +30,8 @@ void MainWindow::on_Convert_pushButton_clicked()
 
         int result = Ulocal_INT / Dollar;
 
-        QString msg = QString("You are successfully convert") + "\n" + "Dollar: " + QString::number(result);
-
+        QString msg = QString("You are successfully convert") + "\n"
+                      + "Dollar: " + QString::number(result);
         QMessageBox::information(this, "Success Message", msg, QMessageBox::Ok);
 
     }
