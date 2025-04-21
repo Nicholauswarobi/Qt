cf7c6ace59805a829aa25ed7efd3aa990c484622
Nicholauswarobi <warobinicholaus@gmail.com>
2025-04-22 01:01:47 +0300

    Currency Converter

--- Currency_Converter/mainwindow.cpp
+++ Currency_Converter/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include "QMessageBox"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -28,6 +29,7 @@ void MainWindow::on_Convert_pushButton_clicked()
     if (UInternational == "Dollar"){
 
         int result = Ulocal_INT / Dollar;
+
     }
 }
 
