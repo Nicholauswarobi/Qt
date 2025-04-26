327d2855f1e21c131c717eba73211e30b5fe37de
Nicholauswarobi <warobinicholaus@gmail.com>
2025-04-26 16:33:01 +0300

    Updated

--- Finance_Management_System/expensedialog.cpp
+++ Finance_Management_System/expensedialog.cpp
@@ -6,9 +6,23 @@ ExpenseDialog::ExpenseDialog(QWidget *parent)
     , ui(new Ui::ExpenseDialog)
 {
     ui->setupUi(this);
+    connect(ui->Expense_Submit_pushButton, &QPushButton::clicked, this,
+            &ExpenseDialog::on_Expense_Submit_Fun);
 }
 
 ExpenseDialog::~ExpenseDialog()
 {
     delete ui;
 }
+
+void ExpenseDialog::on_Expense_Submit_Fun()
+{
+    QString ExpenseType = ui->Expense_Type_lineEdit->text();
+    QString ExpenseAmount = ui->Expense_Amount_lineEdit->text();
+
+    emit getExpenseValue(ExpenseType, ExpenseAmount);
+
+    ui->Expense_Type_lineEdit->clear();
+    ui->Expense_Amount_lineEdit->clear();
+}
+
--- Finance_Management_System/expensedialog.h
+++ Finance_Management_System/expensedialog.h
@@ -15,6 +15,12 @@ public:
     explicit ExpenseDialog(QWidget *parent = nullptr);
     ~ExpenseDialog();
 
+signals:
+    void getExpenseValue(const QString &EType, const QString &EAmount);
+
+private slots:
+    void on_Expense_Submit_Fun();
+
 private:
     Ui::ExpenseDialog *ui;
 };
